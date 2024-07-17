f5a503a42c5b972cd346b8c44ccd44c47a76f88b
Yakiv Lobach <lobachyakiv@gmail.com>
2024-07-18 00:23:15 +0300

    exercise-errors

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
     pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
+    return {
+        "errors":[format_linter_error(error) for error in errors],
+        "path":file_path,
+        "status":"failed" if [format_linter_error(error) for error in errors] else "passed"
+    }
     pass
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
+    return [format_single_linter_file(file_path, errors)
+            for file_path, errors in linter_report.items()]
     pass
