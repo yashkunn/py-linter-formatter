e04f2dfbcf6c5f5f9b52b7113cd186c64d429667
Yakiv Lobach <lobachyakiv@gmail.com>
2024-07-18 00:33:34 +0300

    exercise-errors

--- app/main.py
+++ app/main.py
@@ -11,9 +11,9 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors":[format_linter_error(error) for error in errors],
-        "path":file_path,
-        "status":"failed" if [format_linter_error(error) for error in errors] else "passed"
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if [format_linter_error(error) for error in errors] else "passed"
     }
     pass
 
