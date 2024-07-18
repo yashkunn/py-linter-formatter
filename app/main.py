d1a352b61f5a35f7600411a3a9342da12c70043e
Yakiv Lobach <lobachyakiv@gmail.com>
2024-07-18 17:54:51 +0300

    finaly_exercise-errors

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+# formats the error dict according to the condition
 def format_linter_error(error: dict) -> dict:
     return {
         "line": error["line_number"],
@@ -6,20 +7,19 @@ def format_linter_error(error: dict) -> dict:
         "name": error["code"],
         "source": "flake8"
     }
-    pass
 
 
+# checks for list content and sets the appropriate status and handles errors with the first method
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed" if [format_linter_error(error)
-                               for error in errors] else "passed"
-    }
-    pass
+        "status": "failed" if errors else "passed"}
 
 
+# calls a second function that handles each error
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path, errors)
-            for file_path, errors in linter_report.items()]
-    pass
+    return [
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
+    ]
